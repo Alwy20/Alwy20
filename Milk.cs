ffcc3cdb63ff8057370f06dc213f577253f4d910
Alwy20 <62243241+Alwy20@users.noreply.github.com>
2020-03-30 19:58:21 +0700

    Add files via upload

---
+++ Milk.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCoffeMechine
+{
+    class Milk
+    {
+        private int ml = 0;
+        private int oneCupCoffe = 25;
+
+        public Milk(int ml)
+        {
+            this.ml = ml;
+        }
+
+        public Boolean isAvailable()
+        {
+            return this.ml >= this.oneCupCoffe;
+        }
+
+        public void makeOneCup()
+        {
+            this.ml = this.ml - oneCupCoffe;
+        }
+
+        public int getml()
+        {
+            return this.ml;
+        }
+    }
+}
